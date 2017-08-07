2fdfe1e73f3b826147a9df1572a2910515261199
vasco <vastur@gmail.com>
2017-08-06 22:23:42 -0700

    Kapsule initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Kapsule
+
+A Simple Web Component library, inspired by the [reusable charts pattern](https://bost.ocks.org/mike/chart/) commonly found in [D3](https://d3js.org/) components.
+
+[![NPM](https://nodei.co/npm/kapsule.png?compact=true)](https://nodei.co/npm/kapsule/)
+
+## Quick start
+
+```
+import Kapsule from 'kapsule';
+```
+or
+```
+var Kapsule = require('kapsule');
+```
+or even
+```
+<script src="//unpkg.com/kapsule/dist/kapsule.min.js"></script>
+```
+
+## Usage example
+
+### Define the component
+```
+const ColoredText = Kapsule({
+    
+    props: {
+        color: { defaultVal: 'red' },
+        text: {}
+    },
+    
+    init: (domElement, state) => {
+        state.elem = document.createElement('span');
+        domElement.appendChild(state.elem);
+    },
+    
+    update: state => {
+        state.elem.style.color = state.color;
+        state.elem.textContent = state.text;
+    }
+
+});
+```
+
+### Instantiate the component
+
+```
+let myText = ColoredText();
+```
+
+### Render
+
+```
+myText(<myDOMElement>)
+    .color('blue')
+    .text('foo');
+```
+
+## How to build
+
+```
+npm install
+npm run build
+```
---
+++ index.js
@@ -0,0 +1,84 @@
+class Prop {
+	constructor(name, { defaultVal = null, triggerUpdate = true, onChange = (newVal, state) => {}}) {
+		this.name = name;
+		this.defaultVal = defaultVal;
+		this.triggerUpdate = triggerUpdate;
+		this.onChange = onChange;
+	}
+}
+
+export default function ({
+		props: rawProps = {},
+		methods = {},
+		init: initFn = (() => {}),
+		update: updateFn = (() => {})
+	}) {
+
+	// Parse props into Prop instances
+	const props = Object.keys(rawProps).map(propName =>
+		new Prop(propName, rawProps[propName])
+	);
+
+	return function() {
+
+		// Holds component state
+		let state = {
+			initialised: false
+		};
+
+		// Component constructor
+		function comp(nodeElement) {
+			initStatic(nodeElement);
+			digest();
+
+			return comp;
+		}
+
+		// Getter/setter methods
+		props.forEach(prop => {
+			comp[prop.name] = getSetProp(prop.name, prop.triggerUpdate, prop.onChange);
+			state[prop.name] = prop.defaultVal;
+			prop.onChange(prop.defaultVal, state);
+
+			function getSetProp(prop, redigest = false,  onChange = (newVal, state) => {}) {
+				return _ => {
+					if (!arguments.length) { return state[prop] }
+					state[prop] = _;
+					onChange(_, state);
+					if (redigest) { digest(); }
+					return comp;
+				}
+			}
+		});
+
+		// Other methods
+		Object.keys(methods).forEach(methodName => {
+			comp[methodName] = (...args) => methods[methodName](state, ...args);
+		});
+
+		// Reset all component props to their default value
+		comp.resetProps = function() {
+			props.forEach(prop => {
+				state[prop.name] = prop.defaultVal;
+				prop.onChange(prop.defaultVal, state);
+			});
+			digest();	// Re-digest after resetting props
+
+			return comp;
+		};
+
+		//
+
+		function initStatic(nodeElement) {
+			initFn(nodeElement, state);
+			state.initialised = true;
+		}
+
+		function digest() {
+			if (!state.initialised) { return; }
+			updateFn(state);
+		}
+
+		return comp;
+	}
+}
