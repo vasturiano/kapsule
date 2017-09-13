c440532c59652d4cc8ca32b661f6734f5fd41cbf
vasco <vasco@cloudflare.com>
2017-09-12 21:22:17 -0700

    Migrate to webpack+babel

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'kapsule.js',
+        libraryTarget: 'umd',
+        library: 'Kapsule'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
